5c4c5efc771a3c8470cd201c556cb693865834f3
patelsatyamgit <108532808+patelsatyamgit@users.noreply.github.com>
2023-10-04 20:01:17 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:"https://finalstudynotion.onrender.com",
+        origin:"*",
         credentials:true,
 
     })
